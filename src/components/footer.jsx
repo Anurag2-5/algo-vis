b80b4d855354ed6670e094056ce38ff15767ae06
Anurag2-5 <anurahjadhav1169@gmail.com>
2023-10-14 00:59:30 +0530

    first commit

---
+++ src/components/footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// Footer Component
+const Footer = () => {
+    return (
+        <footer>
+            <p>
+                Created with <i className="fa fa-heart" style={{color: "#FF5252"}}></i> by
+                <a href="https://www.linkedin.com/in/anurag-jadhav-b83a99201/" target="_blank" rel="noopener noreferrer" className="link">
+                    <span> </span>
+                   Anurag Jadhav
+                </a>
+            </p>
+        </footer>
+    );
+}
+ 
+export default Footer;
